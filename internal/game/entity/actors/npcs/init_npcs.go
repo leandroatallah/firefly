d6a3d6698566852a88c2a257ad3b717851d88312
Leandro Atallah <leandroatallah@gmail.com>
2026-03-30 15:36:28 +0100

    refactor(npcs): remove princess NPC

--- internal/game/entity/actors/npcs/init_npcs.go
+++ internal/game/entity/actors/npcs/init_npcs.go
@@ -10,19 +10,11 @@ import (
 )
 
 const (
-	PrincessNpcType npcs.NpcType = "PRINCESS"
-	ClimberNpcType  npcs.NpcType = "CLIMBER"
+	ClimberNpcType npcs.NpcType = "CLIMBER"
 )
 
 func InitNpcMap(ctx *app.AppContext) npcs.NpcMap[platformer.PlatformerActorEntity] {
 	npcMap := map[npcs.NpcType]func(x, y int, id string) platformer.PlatformerActorEntity{
-		PrincessNpcType: func(x, y int, id string) platformer.PlatformerActorEntity {
-			npc, err := NewPrincess(ctx, x, y, id)
-			if err != nil {
-				log.Fatal(err)
-			}
-			return npc
-		},
 		ClimberNpcType: func(x, y int, id string) platformer.PlatformerActorEntity {
 			npc, err := gameplayer.NewClimberPlayer(ctx)
 			if err != nil {
